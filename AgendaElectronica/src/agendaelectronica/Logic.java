00ff6661140c1aaf6f9ffd7e6e99f1cab7a7b17f
arielrb <86982422+arielrb@users.noreply.github.com>
2022-01-12 19:05:37 -0300

    Add files via upload

---
+++ AgendaElectronica/src/agendaelectronica/Logic.java
@@ -0,0 +1,17 @@
+package agendaelectronica;
+import Vistas.Principal;
+public class Logic 
+{
+
+    public static void main(String[] args)
+    {
+        //Instanciamos el objeto principal (el javaFrame)
+        Principal pantalla = new Principal();
+        //Primero la hacemos visible y despues la ponemos en el centro
+        pantalla.setVisible(true);
+        pantalla.setLocationRelativeTo(null); 
+        
+        
+    }
+    
+}
